6ce2dff1f121311efe71f68963de9968da24706e
Debby <124309163+deborah-boat@users.noreply.github.com>
2023-05-09 20:47:52 +0200

    Update server.js

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,6 +10,29 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    
+        if(req.method === 'GET' && req.url === '/state'){
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify({state}));
+    }else if(req.method === 'GET' && req.url === '/add'){
+      state = state + 1;
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify({state}));
+    }else if(req.method === 'GET' && req.url === '/subtract'){
+      state = state - 1;
+      res.writeHead(200,{'Content-Type': 'application/json'});
+      res.end(JSON.stringify({state}));
+    }else if(req.method === 'GET' && req.url === '/reset'){
+      state = 10;
+      res.writeHead(200,{'Content-Type': 'application/json'});
+      res.end(JSON.stringify({state}));
+
+    }else{
+     res.writeHead(404,{'Content-Type': 'application/json'});
+      res.end(JSON.stringify({error:'Not found'}));
+
+    }
+
   });
 
   return server;
